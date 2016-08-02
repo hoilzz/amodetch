ce4aa2bbdb467e836036b5d5af3df7759db36ba3
Ho Ruby <qqwq9999@gmail.com>
2016-08-02 13:57:24 +0900

    hotfix-facebook login

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+OmniAuth.config.logger = Rails.logger
+
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+
+  provider :facebook, ENV["facebook_app_id"], ENV["facebook_app_secret"]
+end 
